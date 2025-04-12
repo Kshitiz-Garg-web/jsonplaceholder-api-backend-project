3d140565831e609a485c34aef003dc4988913bd4
Kshitiz-Garg-web <kshitizgarg456@gmail.com>
2025-04-12 13:05:50 +0530

    root req, 404 & error handler setup

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: Welcome_Msg,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json("404 found - wrong req ");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json("something went wrong in server");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`server running 🚀 on http://localhost:${PORT}`);
